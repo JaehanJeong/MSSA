9a8c2b4d81a310f2cd91fbfed0e7db0971722f4a
learnAndroid <jaehan.jeong24@gmail.com>
2026-05-04 13:00:49 -0700

    added week 5

---
+++ Week 5/Mod1/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Linq;
+
+namespace Mod1
+{
+    internal class Class1
+    {
+        string title;
+        Public Document(string t)
+        {
+            this.title = t;
+        }
+        public string Title
+        {
+            get
+            {
+                return this.title; 
+            }
+        }
+        public string Content
+        {
+            get
+            { 
+
+            }
+
+        }
+
+    }
+}
---
+++ Week 5/Mod1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection.Metadata;
+
+namespace Mod1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Document obj = new Document("Resume", "Qualifications..");
+        }
+    }
+}
