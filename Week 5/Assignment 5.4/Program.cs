2dfdc14e3a112dd399d21a57814e4d73bd52fd22
learnAndroid <jaehan.jeong24@gmail.com>
2026-05-07 14:55:59 -0700

    Assignment 5.4 added

---
+++ Week 5/Assignment 5.4/Program.cs
@@ -0,0 +1,73 @@
+﻿//Write a program in C# Sharp to display the individual digits of
+//a given number using recursion.
+
+int number = 1234;
+void DisplayDigits(int number)
+{
+    //Base case - Stop when we're done chopping all digits.
+    if (number == 0) return;
+
+    //Explored log10, and convert to string ways, but this way seems best overall.
+
+    //Step 1: Process the number, 1 digit at a time. (ex: 1234 -> 123 -> 12 -> 1)
+    //Push each state onto the call stack.
+    //Call itself again without the last digit.
+    DisplayDigits(number / 10);
+
+
+    // Step 2: Unwinding the Stack
+    // As the functions finish and "pop" off the stack, we print the remainder.
+    // The first one to finish is the one that received '1', so it prints first.
+    // Writes 
+    Console.Write($"{number % 10} ");
+}
+DisplayDigits(number);
+Console.WriteLine();
+//Write a C# Sharp program to find the sum of the right diagonals of a matrix.
+//Have the user input the size of the square matrix.
+int userInput = 0;
+while(true)
+{
+    Console.WriteLine("Please input the size of the square matrix.");
+    if (int.TryParse(Console.ReadLine(), out userInput) && userInput > 0)
+        break;
+    Console.WriteLine("Please enter a valid positive whole integer.");
+}
+
+//Initialize the matrix with given size
+int[,] matrix = new int[userInput, userInput];
+
+
+//Let them fill up matrices
+Console.WriteLine("Input elements in the first matrix:");
+for (int i = 0; i< userInput; i++)
+{
+    for (int j = 0; j< userInput; j++)
+    {
+        Console.Write($"element - [{i}],[{j}]] : ");
+        matrix[i, j] = int.Parse(Console.ReadLine());
+    }
+}
+
+
+//Print out the matrix by looping.
+Console.WriteLine("\nThe matrix is : \n");
+for(int i = 0;  i < userInput; i++)
+{
+    for(int j = 0; j< userInput; j++)
+    {
+        Console.Write(matrix[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+
+
+// Sum of right diagonal logic
+int sum = 0;
+for (int i = 0; i< userInput; i++) // Loop as many as userInput says to;
+{
+    //Add to sum: 
+    sum += matrix[i, i];
+}
+
+Console.WriteLine($"Addition of the right Diagonal element is: {sum}");
---
+++ Week 5/Mod6SearchAlgorithms/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace Mod6SearchAlgorithms
+{
+    internal class Program
+    {
+        // O(n)
+        // Constant space complexity (we aren't declaring extra stuff)
+        static bool LinearSearch(int[]arr, int target, out int index)
+        {//out int index is a reference variable. You cant input value 
+            for(int i = 0; i<arr.Length; i++)
+            {
+                if (arr[i] == target)
+                {
+                    index = i;
+                    return true;
+                }
+            }
+            index = -1;
+            return false;
+        }
+
+        static int BinarySearch(int[]arr,int target)
+        {
+            int left = 0;
+            int right = arr.Length - 1;
+            int mid = 0;
+            while(left<=right) // Terminating condition where they crossed each other
+            {
+                mid = (left+right)/2;
+                if (target == mid) return mid; // If mid = target, we're done.
+                else if (target < arr[mid]) right = mid - 1; // If target is smaller than mid, we narrow the right bound.
+                else left = mid + 1; // Otherwise, (If target is bigger than mid), we raise the lower bound
+
+            }
+            return -1;
+        }
+        static void Main(string[] args)
+        {
+            
+            if (LinearSearch(new int[] { 23, 34, 12, 45, 67, 444 }, 5555, out int index))
+            {
+                Console.WriteLine($"Element is found at {index}");
+            }
+            else { Console.WriteLine("Element was not found."); }
+
+            Console.WriteLine("Binary Search");
+            int val = BinarySearch(new int[] { 12, 34, 56, 788, 899 }, 56);
+            if (val >= 0) // Make sure it's sorted. >=0 Ensures that we found it
+            {
+                Console.WriteLine($"Number found. at {val}");
+            }
+            else
+                Console.WriteLine("Number not found :(");
+        }
+    }
+}
