9823dc3eea475684cf0c27807388e38d1a5c9df9
learnAndroid <jaehan.jeong24@gmail.com>
2026-05-11 22:34:03 -0700

    Added Valid Anagram

---
+++ LeetCode/Valid Anagram/MyAttempt.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Valid_Anagram
+{
+    public class MyAttempt
+    {
+        public bool IsAnagram(string s, string t)
+        {
+            Dictionary<char, int> frequency = new Dictionary<char, int>();
+
+            if (s.Length != t.Length) return false;
+
+            for (int i = 0; i < s.Length; i++)
+            {// 3a, 1n, 1g, 1r, 1m
+                if (!frequency.ContainsKey('i'))
+                {
+                    frequency['i'] = 1;
+                }
+                else //(frequency.ContainsKey('i'))
+                {
+                    frequency['i']++;
+                }
+            }
+
+            for (int j = 0; j < t.Length; j++)
+            {
+                // if(!frequency.ContainsKey('j'))
+                // {
+                //     return false;
+                // }
+                if (frequency.ContainsKey('j'))
+                {
+                    frequency['j']--;
+                }
+                if (frequency.['j'] < 0)
+                {
+                    return false;
+                }
+
+            }
+            return true;
+        }
+    }
+}
---
+++ LeetCode/Valid Anagram/Solution.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Valid_Anagram
+{
+    public class Solution
+    {
+        public bool IsAnagram(string s, string t)
+        {
+            if (s.Length != t.Length) return false;
+
+            Dictionary<char, int> frequency = new Dictionary<char, int>();
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                char c = s[i]; // Get the actual character from the string
+                if (!frequency.ContainsKey(c))
+                {
+                    frequency[c] = 1;
+                }
+                else
+                {
+                    frequency[c]++;
+                }
+            }
+
+            for (int j = 0; j < t.Length; j++)
+            {
+                char c = t[j];
+                if (!frequency.ContainsKey(c))
+                {
+                    return false; // Character in 't' wasn't in 's'
+                }
+
+                frequency[c]--;
+
+                if (frequency[c] < 0)
+                {
+                    return false; // 't' has more of this character than 's' does
+                }
+            }
+            return true;
+        }
+    }
+}
