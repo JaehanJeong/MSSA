dea431686c2dbce4fc3158cd6935d57c5d4c55ef
learnAndroid <jaehan.jeong24@gmail.com>
2026-05-12 07:41:08 -0700

    Twosum, Contains Duplicate, Buy sell stock leetcodes added

---
+++ LeetCode/Best Time to Buy and Sell Stock/Solution.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Best_Time_to_Buy_and_Sell_Stock
+{
+    public class Solution
+    {
+        public int MaxProfit(int[] prices)
+        {
+            int bestProfit = 0;
+            int lowestPrice = prices[0];
+
+            for (int day = 1; day < prices.Length; day++)
+            {
+                if (prices[day] < lowestPrice)
+                {
+                    lowestPrice = prices[day];
+                }
+
+                int profit = prices[day] - lowestPrice;
+
+                if (profit > bestProfit)
+                {
+                    bestProfit = profit;
+                }
+            }
+
+            return bestProfit;
+        }
+    }
+}
---
+++ LeetCode/Contains Duplicate/Solution.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Contains_Duplicate
+{
+    public class Solution
+    {
+        public bool ContainsDuplicate(int[] nums)
+        {
+            HashSet<int> seen = new HashSet<int>();
+
+            foreach (int num in nums)
+            {
+                if (!seen.Add(num)) return true;
+            }
+
+            return false;
+        }
+    }
+}
