f1f663c680a91cbf6bbd31c716c11b519bed9935
learnAndroid <jaehan.jeong24@gmail.com>
2026-05-19 22:20:27 -0700

    Merge two sorted lists

---
+++ LeetCode/Merge Two Sorted Lists/Class1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Merge_Two_Sorted_Lists
+{
+    public class Solution
+    {
+        public ListNode MergeTwoLists(ListNode list1, ListNode list2)
+        {
+            ListNode dummy = new(0);
+            ListNode current = dummy;
+
+            while (list1 != null && list2 != null)
+            {
+                if (list1.val < list2.val)
+                {
+                    current.next = list1;
+                    list1 = list1.next;
+                }
+                else
+                {
+                    current.next = list2;
+                    list2 = list2.next;
+                }
+                current = current.next;
+            }
+            if (list1 == null)
+            {
+                current.next = list2;
+            }
+            if (list2 == null)
+            {
+                current.next = list1;
+            }
+            return dummy.next;
+        }
+    }
+}
