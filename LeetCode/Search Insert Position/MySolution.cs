474ac632285e5153afebcb100e44baf340eae1ad
learnAndroid <jaehan.jeong24@gmail.com>
2026-05-25 14:58:40 -0700

    Search Insert Position added

---
+++ LeetCode/Search Insert Position/MySolution.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Search_Insert_Position
+{
+    public class Solution
+    {
+        public int SearchInsert(int[] nums, int target)
+        {
+            int targetLocation = 0;
+            if (target < nums[0]) { return 0; }
+            if (target > nums[nums.Length - 1]) { return nums.Length; }
+            //int i = 0;
+            while (target > nums[targetLocation])
+            {
+                targetLocation++;
+            }
+            return targetLocation;
+        }
+    }
+}
---
+++ LeetCode/Search Insert Position/Solution.cs
@@ -0,0 +1,22 @@
+﻿public class Solution
+{
+    public int SearchInsert(int[] nums, int target)
+    {
+        int left = 0, right = nums.Length - 1;
+
+        while (left <= right)
+        {
+            int mid = left + (right - left) / 2; // avoids int overflow vs (left+right)/2
+
+            if (nums[mid] == target)
+                return mid;
+            else if (nums[mid] < target)
+                left = mid + 1;
+            else
+                right = mid - 1;
+        }
+
+        // When not found, 'left' is exactly where target would be inserted
+        return left;
+    }
+}
