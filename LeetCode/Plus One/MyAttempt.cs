229b909ddc0f709f3448279b4914a4a0c2b5e52c
learnAndroid <jaehan.jeong24@gmail.com>
2026-05-27 07:51:04 -0700

    Plus One

---
+++ LeetCode/Plus One/MyAttempt.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Plus_One
+{
+    public class Solution
+    {
+        public int[] PlusOne(int[] digits)
+        {
+            //Unsure how to address edge case of needing to increase the array size.
+            int i = 1;
+            int index = digits.Length - i;
+            if (digits[digits.Length - 1] + 1 == 10)
+            {
+                //If all digits are 9's, then add a new digit by creating a new array or resize?
+                //if()
+
+                digits[digits.Length - 1] = 0;
+                while (index > 0 && digits[digits.Length - index] == 9)
+                {
+                    digits[digits.Length - index] = 0;
+                    i++;
+                }
+                if (index == 0)
+                {
+                    digits[0] = 1;
+                }
+
+            }
+            else
+            {
+                digits[digits.Length - 1]++;
+            }
+            return digits;
+
+        }
+    }
+}
---
+++ LeetCode/Plus One/Solution.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Plus_One
+{
+    public class Solution
+    {
+        public int[] PlusOne(int[] digits)
+        {
+            for (int i = digits.Length - 1; i >= 0; i--)
+            {
+                if (digits[i] < 9)
+                {
+                    digits[i]++;
+                    return digits;       // No carry, we're done
+                }
+                digits[i] = 0;          // Was 9, becomes 0, carry continues
+            }
+
+            // If we're here, all digits were 9 (e.g. [9,9,9] → [1,0,0,0])
+            int[] result = new int[digits.Length + 1];
+            result[0] = 1;
+            return result;
+        }
+    }
+}
