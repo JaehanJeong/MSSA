dd2312cafbaee95af2cecd51589f0f05ae0d2c0e
learnAndroid <jaehan.jeong24@gmail.com>
2026-05-31 16:14:41 -0700

    Sqrt and Untangled

---
+++ LeetCode/Sqrt(x)/MyAttempt.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sqrt_x_
+{
+    public class MyAttempt
+    {
+        public int MySqrt(int x)
+        {
+            int y = 0; // The result we will return
+                       //Edge case - if x is 0 then just finish
+            if (x == 0) return y;
+
+
+            while (x > 0)
+            {
+                y = x / 2; //Start by cutting in half
+                if (y * y == x) { return y; } // Somehow if it's exact sqrt then return.
+                if (y * y > x) { x /= 2; } // If bigger, try again chop in half.
+                                           // Problem is... we could get to where x is super tiny but continues without finding the solution.
+                                           // Also this changes the x... i think.. potential for critical error?
+                                           //Not sure how to find the decimals that would work here below.
+                                           //if(y * y < x) {}
+
+            }
+            return y;
+        }
+    }
+    // Lets say x = 16
+}
---
+++ LeetCode/Sqrt(x)/Solution.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sqrt_x_
+{
+    public class Solution
+    {
+        public int MySqrt(int x)
+        {
+            if (x == 0) return 0;
+
+            long lo = 1, hi = x;
+
+            while (lo <= hi)
+            {
+                long mid = lo + (hi - lo) / 2; // avoids overflow
+                long sq = mid * mid;
+
+                if (sq == x) return (int)mid;
+                else if (sq < x) lo = mid + 1;  // too small, search right
+                else hi = mid - 1;  // too big, search left
+            }
+
+            // hi is now the floor of sqrt(x)
+            return (int)hi;
+        }
+    }
+}
