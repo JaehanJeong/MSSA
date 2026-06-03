b996c057607f92755b99ade0270360d8802a15fe
learnAndroid <jaehan.jeong24@gmail.com>
2026-06-02 18:53:02 -0700

    merge sorted array

---
+++ LeetCode/Merge Sorted Array/MyAttempt.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Merge_Sorted_Array
+{
+    public class MyAttempt
+    {
+        public void Merge(int[] nums1, int m, int[] nums2, int n)
+        {
+            //Taking care of edge cases
+            if (nums1.Length == 0 && nums2.Length == 0) { nums1 = []; }
+            if (nums1.Length == 0) { nums1 = nums2; }
+            if (nums2.Length == 0) { nums1 = nums1; }
+
+            //One pointer for nums1
+            //Another for nums2
+            int i = 0;
+            int j = 0;
+            //Something's wrong about the condition
+            while (i < nums1.Length - 1 && j < nums2.Length - 1)
+            {
+                if (nums1[i] > nums2[j])
+                {
+                    //we're overwriting so something is going wrong here.
+                    nums1[i] = nums2[j];
+                    j++;
+                }
+                i++;
+            }
+            Console.WriteLine(string.Join(", ", nums1));
+        }
+    }
+}
---
+++ LeetCode/Merge Sorted Array/Solution.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Merge_Sorted_Array
+{
+    internal class Solution
+    {
+        public void Merge(int[] nums1, int m, int[] nums2, int n)
+        {
+            int i = m - 1;      // last real element in nums1
+            int j = n - 1;      // last element in nums2
+            int k = m + n - 1;  // last slot in nums1
+
+            while (j >= 0)
+            {
+                if (i >= 0 && nums1[i] > nums2[j])
+                {
+                    nums1[k--] = nums1[i--];
+                }
+                else
+                {
+                    nums1[k--] = nums2[j--];
+                }
+            }
+        }
+    }
+}
