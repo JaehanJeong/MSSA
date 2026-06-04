daa48a14c7c016c3b56f4eb2fc5160e3f62e01e7
learnAndroid <jaehan.jeong24@gmail.com>
2026-06-03 20:10:14 -0700

    Binary Tree Inorder Traversal

---
+++ LeetCode/Binary Tree Inorder Traversal/Iterative.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Binary_Tree_Inorder_Traversal
+{
+    public class Solution
+    {
+        public IList<int> InorderTraversal(TreeNode root)
+        {
+            List<int> result = new List<int>();
+            Stack<TreeNode> stack = new Stack<TreeNode>();
+            TreeNode current = root;
+
+            while (current != null || stack.Count > 0)
+            {
+                while (current != null)
+                {
+                    stack.Push(current);
+                    current = current.left;
+                }
+
+                current = stack.Pop();
+                result.Add(current.val);
+                current = current.right;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ LeetCode/Binary Tree Inorder Traversal/Recursive.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Binary_Tree_Inorder_Traversal
+{
+    public class Solution
+    {
+        public IList<int> InorderTraversal(TreeNode root)
+        {
+            List<int> result = new List<int>();
+            Traverse(root, result);
+            return result;
+        }
+
+        private void Traverse(TreeNode node, List<int> result)
+        {
+            if (node == null) return;
+
+            Traverse(node.left, result);    // go left
+            result.Add(node.val);           // process current node
+            Traverse(node.right, result);   // go right
+        }
+    }
+}
