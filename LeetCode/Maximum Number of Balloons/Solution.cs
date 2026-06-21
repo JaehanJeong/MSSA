59b9ec8d7b976a9bca919535455fb7a485621d3c
learnAndroid <jaehan.jeong24@gmail.com>
2026-06-21 10:05:28 -0700

    Maximum Number of Balloons

---
+++ LeetCode/Maximum Number of Balloons/Solution.cs
@@ -0,0 +1,34 @@
+﻿public class Solution
+{
+    public int MaxNumberOfBalloons(string text)
+    {
+        //Start with dictionary to keep count of each character
+        Dictionary<char, int> charCount = new();
+
+        // Either add to the dictionary or increment
+        foreach (char c in text)
+        {
+            if (!charCount.ContainsKey(c))
+            {
+                charCount[c] = 1;
+            }
+            else
+            {
+                charCount[c]++;
+            }
+        }
+        //Get counts (if possible) of each character
+        charCount.TryGetValue('b', out int bCount);
+        charCount.TryGetValue('a', out int aCount);
+        charCount.TryGetValue('l', out int lCount);
+        charCount.TryGetValue('o', out int oCount);
+        charCount.TryGetValue('n', out int nCount);
+
+        //Organizing our data. Dividing 2 since each balloon takes 2 of L and O's.
+        int[] balloonCounts = { bCount, aCount, lCount / 2, oCount / 2, nCount };
+        //We can only make as many as the lowest count, so spit that out.
+        int result = balloonCounts.Min();
+
+        return result;
+    }
+}
