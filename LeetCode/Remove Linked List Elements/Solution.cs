017563c9c2175c061828d128d5ba2c4e55b1e82b
learnAndroid <jaehan.jeong24@gmail.com>
2026-06-23 20:36:04 -0700

    Remove Linked List Elements

---
+++ LeetCode/Remove Linked List Elements/Solution.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Remove_Linked_List_Elements
+{
+    public ListNode RemoveElements(ListNode head, int givenValue)
+    {
+        // If head is null or head has the bad value, redirect to the next node.
+        while (head != null && head.val == givenValue)
+        {
+            head = head.next;
+        }
+
+        // Officially start our head / linked list.
+        ListNode current = head;
+
+        // Assuming we aren't at the very end nor penultimate node,
+        while (current != null && current.next != null)
+        {
+            //If our next node is the bad value
+            if (current.next.val == givenValue)
+            {
+                // redirect it to the one after.
+                current.next = current.next.next;
+            }
+            else
+            {
+                // If nothing's wrong then continue mission
+                current = current.next;
+            }
+        }
+        //Gives the linked list back by pointing to its head.
+        return head;
+    }
+}
---
+++ MAUI/ScoreCalculator/MainPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System.Diagnostics;
+
+namespace ScoreCalculator
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0, totalScore = 0;
+        float averageScore = 0.0f;
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdd_Clicked(object sender, EventArgs e)
+        {
+            if(int.TryParse(txtScoreEntry.Text, out int score))
+            {
+                totalScore += score;
+                lblScoreTotal.Text = $"{totalScore}";
+                count++;
+                lblScoreCount.Text = $"{count}";
+                averageScore = (float)totalScore / count;
+                lblScoreAverage.Text = $"{averageScore:F2}";
+
+            }
+            else
+            {
+                DisplayAlert("invalid input", "Please enter a valid number", "Ok");
+            }
+
+        }
+
+        private void btnReset_Clicked(object sender, EventArgs e)
+        {
+            count = 0;
+            totalScore = 0;
+            averageScore = 0.0f;
+            lblScoreTotal.Text = "0";
+            lblScoreAverage.Text = "0";
+            lblScoreCount.Text = "0";
+            txtScoreEntry.Text = string.Empty;
+
+        }
+
+        private void btnExit_Clicked(object sender, EventArgs e)
+        {
+            Process.GetCurrentProcess().CloseMainWindow();
+
+
+        }
+    }
+}
